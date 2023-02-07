0dfc0d5491d7e44b94bd869db89a524b429876c1
SwiftSkyline <101878793+SwiftSkyline@users.noreply.github.com>
2023-02-07 15:23:31 +0000

    Update messaround.js

--- js/messaround.js
+++ js/messaround.js
@@ -9,8 +9,8 @@ function setup() {
     // smooth the stroke joins
     strokeJoin(ROUND);
     // define branch colours
-    brown1 = color('#AB589D');
-    brown2 = color('#587FAB');
+    brown1 = color('#38D360');
+    brown2 = color('#4BAF64');
     // create a slider
     slider = createSlider(5, 180, 5);
     slider.position(10, 10);
@@ -24,7 +24,7 @@ function draw() {
     resetMatrix();
     // initially translate to the middle of the bottom edge of the canvas
     translate(width / 2, height);
-    branch(100);
+    branch(150);
 }
 function branch(len) { // length of branch to draw
     // the maximum/minimum angle of each branch
@@ -42,35 +42,52 @@ function branch(len) { // length of branch to draw
     if (len > 10) {
         // draw leaf and terminate recursion
         if (len < 15) { 
+            
             console.log("Ferntest")
             // semi-random leaf colour
-            let r = 200 + random(-20, 20);
+            let r = 10 + random(-20, 20);
             let g = 160 + random(-20, 20);
-            let b = 190 + random(-20, 20);
+            let b = 15 + random(-20, 20);
             fill(r, g, b, 90);
             // semi-random leaf size
             let size = 200 + random(40);
            noStroke();
         
         } else {
+            let arm=len*1.1;
+            // length of arm
             push();
             rotate(90);
-            line(0,0,0,-len * 0.5)
-            let spacer=40;
-            let size=25;
-            for(let i=0; i<len*0.5;i+=spacer){
+            line(0,0,0,-arm)
+            let spacer=30;
+            // space between leaves
+            let size=18;
+            // leaf size
+            for(let i=0; i<arm;i+=spacer){
+                let r = 10 + random(-20, 20);
+                let g = 160 + random(-20, 20);
+                let b = 15 + random(-20, 20);
+                fill(r, g, b, 100);
                 ellipse(size,i,size*2,size)
                 ellipse(-size,i,size*2,size)
                 //fern leaves left size
+                noStroke();
+                
             }
             pop();
             push();
             rotate(-90);
-            line(0,0,0,-len * 0.5)
-            for(let i=0; i<len*0.5;i+=spacer){
+            line(0,0,0,-arm)
+            for(let i=0; i<arm;i+=spacer){
+                let r = 10 + random(-20, 20);
+                let g = 160 + random(-20, 20);
+                let b = 15 + random(-20, 20);
+                fill(r, g, b, 100);
                 ellipse(size,i,size*2,size)
                 ellipse(-size,i,size*2,size)
                 //fern leaves right side
+                noStroke();
+              
             }
             pop();
             // branch 1
